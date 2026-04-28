62c338274fdf41a5a011f93b3a2cc0d6ead5b603
machineselling906-ship-it <machineselling906@gmail.com>
2026-04-28 20:04:10 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -475,4 +475,4 @@ class CardModal(discord.ui.Modal, title="💳 Nhập thông tin thẻ"):
 
 def start_bot(): bot.run(TOKEN)
 threading.Thread(target=start_bot, daemon=True).start()
-if __name__ == "__main__": uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+if __name__ == "__main__": uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
